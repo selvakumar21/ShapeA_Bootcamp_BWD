b1996c4af46bc9f944f124bf2ebdabacc7fab2b4
selvakumar21 <79460075+selvakumar21@users.noreply.github.com>
2021-06-27 23:36:28 +0530

    Add files via upload

---
+++ Info3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info3() {
+  return (
+    <div className="note">
+      <h1>Python and Network Security</h1>
+      <p>
+        Here we bring our another free live BOOTCAMP with a hands-on project at
+        the end; Python and Networking Security
+      </p>
+    </div>
+  );
+}
+
+export default Info3;
---
+++ Info4.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info4() {
+  return (
+    <div className="note">
+      <h1>Python and Natural Language Processing</h1>
+      <p>
+        Here we bring our another free live BOOTCAMP with a hands-on project at
+        the end - Foundation of Python and Natural Programming Processing
+      </p>
+    </div>
+  );
+}
+
+export default Info4;
